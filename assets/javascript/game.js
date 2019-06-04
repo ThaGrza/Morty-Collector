f4940e5559bed5a6185e063f6b1e095f84d0c84a
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-03 20:29:07 -0500

    Added random numbers to crystals and targeted score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,23 @@ var random_result;
 var win;
 var lose;
 
+
+
+random_result = Math.floor(Math.random() * 120) + 19;
+console.log(random_result);
+     
+$("#result").html("Random Result: " + random_result);
 // Crystal main logic.
 for (var i = 0; i < 4; i++){
+    var random = Math.floor(Math.random() * 11)+ 1;
+    console.log(random);
     var crystal = $("<div>");
-    crystal.attr("class", "crystal");
+    crystal.attr({
+        "class": "crystal",
+        "data-random": random});
     $(".crystals").append(crystal);
-
 }
+
+// $(".crystal").on("click", function() {
+//     console.log($(this));
+// })
