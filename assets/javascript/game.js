cd6d0cf6716a06c492c8f7c8e424c6374338b8bc
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-03 19:56:04 -0500

    Initial push

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Assigns variables.
+var random_result;
+var win;
+var lose;
+
+// Crystal main logic.
+for (var i = 0; i < 4; i++){
+    var crystal = $("<div>");
+    crystal.attr("class", "crystal");
+    $(".crystals").append(crystal);
+
+}
