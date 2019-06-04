17c2fa70f72c5350b70c8dfed7f9c618f2af3aa3
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-03 21:57:03 -0500

    completed game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,57 @@
 // Assigns variables.
 var random_result;
-var win;
-var lose;
+var win = 0;
+var lose = 0;
+var current = 0;
 
 
-
-random_result = Math.floor(Math.random() * 120) + 19;
+// Generates Targeted score.
+random_result = Math.floor(Math.random() * 101) + 19;
 console.log(random_result);
-     
-$("#result").html("Random Result: " + random_result);
-// Crystal main logic.
-for (var i = 0; i < 4; i++){
-    var random = Math.floor(Math.random() * 11)+ 1;
-    console.log(random);
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": "crystal",
-        "data-random": random});
-    $(".crystals").append(crystal);
+
+// Displays current score and target score.
+$("#target").html("Random Result: " + random_result);
+$("#currentNum").html("Current Score: " + current);
+
+
+
+var startGame = function() {
+    // Clears previous crystals.
+    $('.crystals').empty();
+
+    // Generates crystals with random numbers.
+    for (var i = 0; i < 4; i++){
+        var random = Math.floor(Math.random() * 11)+ 1;
+        console.log(random);
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": "crystal",
+            "data-random": random});
+        $(".crystals").append(crystal);
+    }
 }
 
-// $(".crystal").on("click", function() {
-//     console.log($(this));
-// })
+// Starts game initially.
+startGame();
+
+// Adds targeted crystal value to score counter.
+$(document).on("click", ".crystal", function() {
+    var num = parseInt($(this).attr('data-random'));
+    current += num;
+    if(current > random_result){
+        lose++;
+        $('#lost').html("lost: " + lose);
+        alert("You Lost!!!");
+        current = 0;
+        startGame();
+    }
+    else if (current === random_result) {
+        win++;
+        $('#win').html("won: " + win);
+        alert("You Won!!!");
+        current = 0;
+        startGame();
+
+    }
+    console.log(current);
+});
