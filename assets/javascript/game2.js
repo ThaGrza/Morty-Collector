df9e3c7a7ad6e5849d47f4179ac81b6b878e1862
ThaGrza <andrewgriswold7@gmail.com>
2021-05-31 18:43:57 -0400

    added new morty images / changed onclick

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,34 +1,50 @@
 $(document).ready(function() {
+    const mortys = {
+        morty1: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "../assets/images/mortys/morty1.png",
+        },
+        // morty2: {
+        //     points: Math.floor(Math.random() * 12) + 1,
+        //     image: "assets/images/mortys/morty2.png",
+        // },
+        morty3: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty3.png",
+        },
+        morty4: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty4.png",
+        },
+        // morty5: {
+        //     points: Math.floor(Math.random() * 12) + 1,
+        //     image: "assets/images/mortys/morty5.png",
+        // },
+        // morty6: {
+        //     points: Math.floor(Math.random() * 12) + 1,
+        //     image: "assets/images/mortys/morty6.png",
+        // },
+        morty7: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty7.png",
+        },
+        morty8: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty8.png",
+        },
+        // morty9: {
+        //     points: Math.floor(Math.random() * 12) + 1,
+        //     image: "assets/images/mortys/morty9.png",
+        // },
+        // morty0: {
+        //     points: Math.floor(Math.random() * 12) + 1,
+        //     image: "assets/images/mortys/Morty0.png",
+        // },
+    }
 
+    var currentScore = 0;
 
     function setGameUp(){
-        var playerMatchingNumber = 0;
-        const mortys = {
-            morty1: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty1.png",
-            },
-            morty2: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty2.png",
-            },
-            morty3: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty3.png",
-            },
-            morty4: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty4.png",
-            },
-            morty5: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty5.png",
-            },
-            morty6: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageurl: "assests/images/mortys/morty6.png",
-            },
-        }
         for(let i = 0; i < mortys.length; i++){
             $("#gameArea").append(i[i]);
         }
@@ -38,36 +54,43 @@ $(document).ready(function() {
         generateMortys();
     }
 
+    function renderMortys(){
+        for (var key in mortys){
+        var mortyDiv = $("<div class='mortyButton'>");
+        var mortyImage = $("<img alt='A wild morty appears' class='mortyImage'>").attr("src", mortys[key].image);
+        mortyImage.attr("value", mortys[key].points)
+        mortyDiv.append(mortyImage);
+        $("#mortyArea").append(mortyDiv);
+        }
+    }
+
+
+
     function generateMortySound(sound){
         // picks random soundbite for Morty to say every click
     }
 
-    function generateMortys(){
-        return {
 
-        }
+
+    function generateMortys(){
+    // Decides how many mortys can be in play / in onDeck
+
     }
 
+    function updatePlayerScore(morty){
+        // currentScore += 3;
+        // $("#currentScore").text(currentScore);
+        alert(morty.value);
+    }
+
+
     setGameUp();
+    renderMortys();
 
+    $(".mortyButton").on("click", function(event){
+        updatePlayerScore($(this));
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     // create winning game  / round win logic
 
     // create losing game / round loss logic
