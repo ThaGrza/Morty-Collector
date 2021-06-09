e758ddbb8f2ce0a51c45472a9a14f006854119ac
ThaGrza <andrewgriswold7@gmail.com>
2021-06-09 17:37:34 -0400

    renders new morty on collection of a morty

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -6,26 +6,6 @@ $(document).ready(function() {
             points: Math.floor(Math.random() * 12) + 1,
             image: "../assets/images/mortys/morty1.png",
         },
-        // morty2: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty2.png",
-        // },
-        morty3: {
-            points: Math.floor(Math.random() * 12) + 1,
-            image: "assets/images/mortys/morty3.png",
-        },
-        morty4: {
-            points: Math.floor(Math.random() * 12) + 1,
-            image: "assets/images/mortys/morty4.png",
-        },
-        // morty5: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty5.png",
-        // },
-        // morty6: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty6.png",
-        // },
         morty7: {
             points: Math.floor(Math.random() * 12) + 1,
             image: "assets/images/mortys/morty7.png",
@@ -34,33 +14,54 @@ $(document).ready(function() {
             points: Math.floor(Math.random() * 12) + 1,
             image: "assets/images/mortys/morty8.png",
         },
-        // morty9: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty9.png",
-        // },
-        // morty0: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/Morty0.png",
-        // },
+        morty3: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty3.png",
+        },
+        morty4: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty4.png",
+        },
+        morty2: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty2.png",
+        },
+        morty5: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty5.png",
+        },
+        morty6: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty6.png",
+        },
+        morty9: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty9.png",
+        },
+        morty0: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/Morty0.png",
+        },
         }
     }
+    var mortysInPlay = 0;
 
     function setGameUp(){
         roundReset();
-        // var targetScore = Math.floor(Math.random() * 102) + 19;
         mortys = randomNumMortys();
-        for(let i = 0; i < mortys.length; i++){
-            $("#gameArea").append(i[i]);
-        }
     }
-
+    // create GAME WIN function that keeps track of collected / ondeck mortys and animates screen when 6 are collected.
 
     function renderMortys(){
         for (var key in mortys){
-        var mortyDiv = $("<div class='mortyButton' data-name='" + key + "'>");
-        var mortyImage = $("<img alt='A wild morty appears' class='mortyImage'>").attr("src", mortys[key].image);
-        mortyDiv.append(mortyImage);
-        $("#mortyArea").append(mortyDiv);
+            if(mortysInPlay < 4){
+                var mortyDiv = $("<div class='mortyButton' data-name='" + key + "'>");
+                var mortyImage = $("<img alt='A wild morty appears' class='mortyImage'>").attr("src", mortys[key].image);
+                mortyDiv.append(mortyImage);
+                $("#mortyArea").append(mortyDiv);
+                mortysInPlay += 1;
+                console.log(mortysInPlay);
+            }
         }
     }
 
@@ -74,6 +75,8 @@ $(document).ready(function() {
 
     function generateMortys(morty){
         morty.remove();
+        mortysInPlay -= 1;
+        renderMortys();
         var mortyImage = (mortys[morty.attr("data-name")]).image;
         var mortyCollectedImage = $("<img alt='A Collected Morty' class='collectedMorty'>").attr("src", mortyImage);
         $("#mortysCollected").append(mortyCollectedImage);
@@ -98,10 +101,6 @@ $(document).ready(function() {
 
     $(".mortyButton").on("click", function(event){
         updatePlayerScore($(this));
-        
-        // DELETE LATER
-        console.log("current score " + currentScore);
-        console.log("target score " + targetScore);
     })
 
     // create winning game  / round win logic
