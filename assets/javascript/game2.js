a264541f68ce4990aa3a4d401dae645241cc77c9
ThaGrza <andrewgriswold7@gmail.com>
2021-06-30 17:06:39 -0400

    added new mortys and sound clips for win,loss, and onclick

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 
-    
+    // var rules = document.getElementById(".rulesModal");
+    // rules.show;
+    const winSounds = ["assets/sounds/win/alright.mp3","assets/sounds/win/heckYeah.mp3","assets/sounds/win/thankGod.mp3","assets/sounds/win/waiting.mp3"];
+
+    const lossSounds = ["assets/sounds/loss/absolutely.mp3","assets/sounds/loss/doingOk.mp3",
+    "assets/sounds/loss/moron.mp3","assets/sounds/loss/geez.mp3","assets/sounds/loss/sorry.mp3"];
+
+    const mortySounds = ["assets/sounds/morty/anxiety.mp3","assets/sounds/morty/hello.mp3",
+    "assets/sounds/morty/helpMe.mp3","assets/sounds/morty/howMuch.mp3","assets/sounds/morty/gardener.mp3",
+    "assets/sounds/morty/sleep.mp3","assets/sounds/morty/weird.mp3","assets/sounds/morty/geez.mp3"];
+
     const mortys = {
         morty1: {
             points: Math.floor(Math.random() * 12) + 1,
@@ -22,26 +32,26 @@ $(document).ready(function() {
             points: Math.floor(Math.random() * 12) + 1,
             image: "assets/images/mortys/morty5.png",
         },
-        // morty6: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty1.png",
-        // },
-        // morty7: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty2.png",
-        // },
-        // morty8: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty3.png",
-        // },
-        // morty9: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty4.png",
-        // },
-        // morty10: {
-        //     points: Math.floor(Math.random() * 12) + 1,
-        //     image: "assets/images/mortys/morty5.png",
-        // },
+        morty6: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty6.png",
+        },
+        morty7: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty7.png",
+        },
+        morty8: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty8.png",
+        },
+        morty9: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty9.png",
+        },
+        morty10: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty10.png",
+        },
     }
 
     var onDeckMorty = [];
@@ -76,10 +86,7 @@ $(document).ready(function() {
                 }
         }
     }
-    // *************************************************************************************************************************************************
-    // fails after first render of morty because array is empty, Need to either change the logic of function or add enough Morty's to buffer array.
-    // *************************************************************************************************************************************************
-    
+
     function newMorty(needsMorty){
         console.log(onDeckMorty);
         if(needsMorty === true){
@@ -93,16 +100,29 @@ $(document).ready(function() {
         }
     }
 
-    function generateMortySound(sound){
-        // picks random soundbite for Morty to say every click
+    function generateMortySound(condition){
+        if(condition === 'morty'){
+            let randomMortySound = Math.floor(Math.random() * mortySounds.length);
+            let mortySound = new Audio(mortySounds[randomMortySound]);
+            mortySound.play();
+        }
+        if(condition === 'win'){
+            let winningSound = Math.floor(Math.random() * winSounds.length);
+            let winSound = new Audio(winSounds[winningSound]);
+            winSound.play();
+        };
+        if(condition === 'loss'){
+            let losingSound = Math.floor(Math.random() * lossSounds.length);
+            let lossSound = new Audio(lossSounds[losingSound]);
+            lossSound.play();
+        }
     }
-
-
 
     function collectMorty(morty){
         morty.remove();
         needsMorty = true;
         newMorty(needsMorty);
+        generateMortySound('win');
         var mortyImage = (mortys[morty.attr("data-name")]).image;
         var mortyCollectedImage = $("<img alt='A Collected Morty' class='collectedMorty'>").attr("src", mortyImage);
         $("#mortysCollected").append(mortyCollectedImage);
@@ -127,6 +147,7 @@ $(document).ready(function() {
 
     $("#mortyArea").on("click", '.mortyButton', function(event){
         updatePlayerScore($(this));
+        generateMortySound('morty');
     })
 
     function roundWin(){
@@ -146,6 +167,7 @@ $(document).ready(function() {
             roundWin();
         }
         if (currentScore > targetScore){  
+            generateMortySound('loss');
             roundReset();
             setGameUp();
             alert("You Lost!");
