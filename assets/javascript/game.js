a4eb5b70628f9850fa4fc6ff5180d8c1183d2709
ThaGrza <andrewgriswold7@gmail.com>
2021-07-05 21:24:05 -0400

    fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,138 +1,175 @@
-// Execute this code when the DOM has fully loaded.
-$(document).ready(function() {
+$(document).ready(function() {    
+    const winSounds = ["assets/sounds/win/alright.mp3","assets/sounds/win/heckYeah.mp3","assets/sounds/win/thankGod.mp3","assets/sounds/win/waiting.mp3"];
+    const lossSounds = ["assets/sounds/loss/absolutely.mp3","assets/sounds/loss/doingOk.mp3",
+    "assets/sounds/loss/moron.mp3","assets/sounds/loss/geez.mp3","assets/sounds/loss/sorry.mp3"];
+    
+    // const mortySounds = ["assets/sounds/morty/anxiety.mp3","assets/sounds/morty/hello.mp3",
+    // "assets/sounds/morty/helpMe.mp3","assets/sounds/morty/howMuch.mp3","assets/sounds/morty/gardener.mp3",
+    // "assets/sounds/morty/sleep.mp3","assets/sounds/morty/weird.mp3","assets/sounds/morty/geez.mp3"];
 
-  // The number we will manipulate by clicking crystals. Our "current guess" number.
-  var yourMatchingNumber = 0;
-
-  // Generates the random "target number" we will try to reach.
-  var randomNum = randomNumGen();
-
-  // Setting up our starting variables.
-  var wins = 0;
-  var losses = 0;
-  var crystals;
-  // Function that generates random values for our crystals and returns our crystals object.
-  function randomNumCrystals() {
-    // Crystals object.
-    return {
-      red: {
-        points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/red.jpg",
-      },
-      blue: {
-        points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/blue.jpg"
-      },
-      yellow: {
-        points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/orange.jpg"
-      },
-      green: {
-        points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/green.jpg"
-      }
-    };
-  }
-  
-  // Function to create a random number between 19 and 120.
-  function randomNumGen() {
-    return Math.floor(Math.random() * 102) + 19;
-  }
-
-  // Function that resets the game.
-  function setGame() {
-    // Make our current total number 0.
-    yourMatchingNumber = 0;
-    // Generate random crystal values.
-    crystals = randomNumCrystals();
-    // Generate a random target number and render it to the page.
-    randomNum = randomNumGen();
-    $("#random-area").text(randomNum);
-  }
-
-  // Function that handles updating the page.
-  function updateDom(didUserWin) {
-    $("#win-area").empty();
-
-    // If the user won...
-    if (didUserWin === true) {
-      // Show victory message, restart the game, and render the new "current guess" number.
-      $("#win-area").append($("<p>").text("You won!!"));
-      setGame();
-      renderMatchingNumber();
+    const mortys = {
+        morty1: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty1.png",
+        },
+        morty2: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty2.png",
+        },
+        morty3: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty3.png",
+        },
+        morty4: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty4.png",
+        },
+        morty5: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty5.png",
+        },
+        morty6: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty6.png",
+        },
+        morty7: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty7.png",
+        },
+        morty8: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty8.png",
+        },
+        morty9: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty9.png",
+        },
+        morty10: {
+            points: Math.floor(Math.random() * 12) + 1,
+            image: "assets/images/mortys/morty10.png",
+        },
     }
-    // If the user lost...
-    else if (didUserWin === false) {
-      // Show defeat message, restart the game, and render the new "current guess" number.
-      $("#win-area").append($("<p>").text("You lost!!"));
-      setGame();
-      renderMatchingNumber();
+    
+    var onDeckMorty = [];
+    var needsMorty = false;
+    var mortysInPlay = 0;
+    var wins = 0;
+
+    function setGameUp(){
+        roundReset();
     }
-2
-    // Building our win/loss display and appending it to the page.
-    var wSpan = $("<span>").text(wins);
-    var lSpan = $("<span>").text(losses);
 
-    var pWins = $("<p>").text("Wins: ");
-    var pLosses = $("<p>").text("Losses: ");
-
-    pWins.append(wSpan);
-    pLosses.append(lSpan);
-
-    $("#win-area").append(pWins);
-    $("#win-area").append(pLosses);
-  }
-
-  // Function to render our crystals to the page.
-  function renderCrystals() {
-    for (var key in crystals) {
-      var crystalDiv = $("<div class='crystals-button' data-name='" + key + "'>");
-      var crystalImg = $("<img alt='image' class='crystal-img'>").attr("src", crystals[key].imageUrl);
-      crystalDiv.append(crystalImg);
-      $("#crystal-area").append(crystalDiv);
+    function showRules(){
+        $("#modal").show();
     }
-  }
 
-  // Function to update our "current guess" number. We are passing in the crystal that was clicked as an argument.
-  function updateMatchingNumber(crystal) {
-    // Update our "current guess" number based on which crystal was clicked.
-    yourMatchingNumber += crystals[crystal.attr("data-name")].points;
-  }
-
-  // Function that will render your "current guess" number to the page.
-  function renderMatchingNumber() {
-    var scoreNumDiv = $("<div id='score-number'>").text(yourMatchingNumber);
-    $("#score-area").html();
-    $("#score-area").html(scoreNumDiv);
- }
-
-  // Call our functions to start the game!
-  setGame();
-  updateDom();
-  renderCrystals();
-  renderMatchingNumber();
-
-  // Here we create an on.click event for the crystals.
-  $(".crystals-button").on("click", function(event) {
-    // Update our "current guess" number and re-render it.
-    updateMatchingNumber($(this));
-    renderMatchingNumber();
-
-    // Check to see if we have won or lost.
-    // If our current guess number equals the target number..
-    if (yourMatchingNumber === randomNum) {
-      // Increment wins, restart the game, and update the page.
-      wins++;
-      setGame();
-      updateDom(true);
+    function renderMortys(){
+        for (var key in mortys){
+            if(mortysInPlay < 4){
+                var mortyDiv = $("<div class='mortyButton' data-name='" + key + "'>");
+                var mortyImage = $("<img alt='A wild morty appears' class='mortyImage'>").attr("src", mortys[key].image);
+                mortyDiv.append(mortyImage);
+                $("#mortyArea").append(mortyDiv);
+                mortysInPlay += 1;
+                }else{
+                    var mortyDiv = $("<div class='mortyButton' data-name='" + key + "'>");
+                    var mortyOnDeckImage = $("<img alt='A wild morty appears' class='onDeckMorty'>").attr("src", mortys[key].image);
+                    mortyDiv.append(mortyOnDeckImage);
+                    onDeckMorty.push(mortyDiv);
+                    $("#mortysOnDeck").append(mortyDiv);
+                }
+        }
     }
-    // If our guess number exceeded our target number...
-    else if (yourMatchingNumber > randomNum) {
-      // Increment losses, restart the game, and update the page.
-      losses++;
-      setGame();
-      updateDom(false);
-    }
-  });
 
-});
+
+    function newMorty(needsMorty){
+        console.log(onDeckMorty);
+        if(needsMorty === true){
+            onDeckMorty[0].addClass("mortyImage");
+            onDeckMorty[0].removeClass("onDeckMorty");
+            $("#mortyArea").append(onDeckMorty[0]);
+            onDeckMorty.shift(onDeckMorty[0]);
+            console.log(onDeckMorty.length);
+            needsMorty = false;
+        }else{
+        }
+    }
+
+
+    function generateMortySound(condition){
+        // if(condition === 'morty'){
+        //     let randomMortySound = Math.floor(Math.random() * mortySounds.length);
+        //     let mortySound = new Audio(mortySounds[randomMortySound]);
+        //     mortySound.play();
+        // }
+        if(condition === 'win'){
+            let winningSound = Math.floor(Math.random() * winSounds.length);
+            let winSound = new Audio(winSounds[winningSound]);
+            winSound.play();
+        };
+        if(condition === 'loss'){
+            let losingSound = Math.floor(Math.random() * lossSounds.length);
+            let lossSound = new Audio(lossSounds[losingSound]);
+            lossSound.play();
+        }
+    }
+
+    function collectMorty(morty){
+        morty.remove();
+        needsMorty = true;
+        newMorty(needsMorty);
+        generateMortySound('win');
+        var mortyImage = (mortys[morty.attr("data-name")]).image;
+        var mortyCollectedImage = $("<img alt='A Collected Morty' class='collectedMorty'>").attr("src", mortyImage);
+        $("#mortysCollected").append(mortyCollectedImage);
+    }
+
+    function updatePlayerScore(morty){
+        currentScore += mortys[morty.attr("data-name")].points;
+        $("#currentScore").text(currentScore);
+        winLose(currentScore, morty);
+    }
+
+    function roundReset(){
+        currentScore = 0;
+        targetScore = Math.floor(Math.random() * 102) + 19;
+        $("#currentScore").text(currentScore);
+        $("#targetScore").text(targetScore);
+    }
+
+    setGameUp();
+    renderMortys();
+    showRules();
+
+
+    $("#mortyArea").on("click", '.mortyButton', function(event){
+        updatePlayerScore($(this));
+    })
+
+    $("#modalClose").on("click", function(event){
+        $("#modal").hide();
+    })
+
+    function roundWin(){
+        wins += 1;
+        if(wins === 6){
+            alert("You won the game")
+        }
+    }
+
+    function winLose(playerScore, morty){
+        if (currentScore === targetScore){
+            console.log(needsMorty);
+            collectMorty(morty);
+            roundReset();
+            setGameUp();
+            roundWin();
+        }
+        if (currentScore > targetScore){  
+            generateMortySound('loss');
+            roundReset();
+            setGameUp();
+            alert("You Lost!");
+        }
+    }
+})
